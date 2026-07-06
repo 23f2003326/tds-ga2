3048f574a5aaecfff273eed107033e5be1f74835
Chandan <chandan297003@gmail.com>
2026-07-06 13:03:56 +0530

    Fix CORS

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ALLOWED_ORIGIN],
+    allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
